6ba6e235776d13b32df0d8cffad4e6a540ebca93
Nick Smith <nick.smith@cern.ch>
2018-05-30 02:12:39 +0200

    Prefiring Z?

---
+++ plugins/PrefiringZAna.cc
@@ -0,0 +1,195 @@
+// system include files
+#include <memory>
+
+// user include files
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/one/EDAnalyzer.h"
+
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/Utilities/interface/transform.h"
+
+#include "FWCore/ServiceRegistry/interface/Service.h"
+#include "CommonTools/UtilAlgos/interface/TFileService.h"
+
+#include "CommonTools/Utils/interface/StringCutObjectSelector.h"
+
+#include "DataFormats/PatCandidates/interface/Electron.h"
+#include "DataFormats/VertexReco/interface/VertexFwd.h"
+#include "DataFormats/VertexReco/interface/Vertex.h"
+#include "DataFormats/L1Trigger/interface/EGamma.h"
+#include "DataFormats/PatCandidates/interface/TriggerObjectStandAlone.h"
+#include "DataFormats/PatCandidates/interface/PackedTriggerPrescales.h"
+#include "DataFormats/HLTReco/interface/TriggerTypeDefs.h"
+
+#include "TTree.h"
+
+#include "Math/LorentzVector.h"
+#include "Math/PxPyPzE4D.h"
+// == reco::LorentzVector
+typedef ROOT::Math::LorentzVector<ROOT::Math::PxPyPzE4D<double>> LorentzVector;
+
+namespace {
+  struct EventStruct {
+    Long64_t run;
+    Long64_t lumi;
+    Long64_t event;
+    
+    LorentzVector tag_electron;
+    std::vector<LorentzVector> l1eg_bx0;
+    std::vector<LorentzVector> l1eg_bxm1;
+    std::vector<LorentzVector> l1eg_bxp1;
+  };
+
+  std::vector<const pat::TriggerObjectStandAlone*> getMatchedObjs(const float eta, const float phi, const std::vector<pat::TriggerObjectStandAlone>& trigObjs, const float maxDeltaR=0.1)
+  {
+    std::vector<const pat::TriggerObjectStandAlone*> matchedObjs;
+    const float maxDR2 = maxDeltaR*maxDeltaR;
+    for(auto& trigObj : trigObjs){
+      const float dR2 = reco::deltaR2(eta,phi,trigObj.eta(),trigObj.phi());
+      if(dR2<maxDR2) matchedObjs.push_back(&trigObj);
+    }
+    return matchedObjs;
+  }
+}
+
+class PrefiringZAna : public edm::one::EDAnalyzer<edm::one::SharedResources>  {
+  public:
+    explicit PrefiringZAna(const edm::ParameterSet&);
+    ~PrefiringZAna();
+
+    static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
+
+  private:
+    // virtual void beginJob() override;
+    virtual void analyze(const edm::Event&, const edm::EventSetup&) override;
+    // virtual void endJob() override;
+
+    edm::EDGetTokenT<pat::ElectronCollection> electronToken_;
+    // edm::EDGetTokenT<edm::ValueMap<bool>> mediumIdToken_;
+    edm::EDGetTokenT<reco::VertexCollection> vertexToken_;
+    StringCutObjectSelector<pat::Electron> tagElectronCut_;
+    edm::EDGetTokenT<BXVector<l1t::EGamma>> l1egToken_;
+    edm::EDGetTokenT<pat::TriggerObjectStandAloneCollection> triggerObjectsToken_;
+    edm::EDGetTokenT<pat::PackedTriggerPrescales> triggerPrescalesToken_;
+
+    TTree * tree_;
+    EventStruct event_;
+};
+
+PrefiringZAna::PrefiringZAna(const edm::ParameterSet& iConfig):
+  electronToken_(consumes<pat::ElectronCollection>(iConfig.getParameter<edm::InputTag>("electronSrc"))),
+  //  mediumIdToken_(consumes<edm::ValueMap<bool>>(iConfig.getParameter<edm::InputTag>("mediumId"))),
+  vertexToken_(consumes<reco::VertexCollection>(iConfig.getParameter<edm::InputTag>("vertexSrc"))),
+  tagElectronCut_(iConfig.getParameter<std::string>("tagElectronCut")),
+  l1egToken_(consumes<BXVector<l1t::EGamma>>(iConfig.getParameter<edm::InputTag>("l1egSrc"))),
+  triggerObjectsToken_(consumes<pat::TriggerObjectStandAloneCollection>(iConfig.getParameter<edm::InputTag>("triggerObjects"))),
+  triggerPrescalesToken_(consumes<pat::PackedTriggerPrescales>(iConfig.getParameter<edm::InputTag>("triggerPrescales")))
+{
+  usesResource("TFileService");
+  edm::Service<TFileService> fs;
+
+  tree_ = fs->make<TTree>("tree","Event Summary");
+  tree_->Branch("run", &event_.run);
+  tree_->Branch("lumi", &event_.lumi);
+  tree_->Branch("event", &event_.event);
+  tree_->Branch("tag_electron", &event_.tag_electron);
+  tree_->Branch("l1eg_bx0", &event_.l1eg_bx0);
+  tree_->Branch("l1eg_bxm1", &event_.l1eg_bxm1);
+  tree_->Branch("l1eg_bxp1", &event_.l1eg_bxp1);
+}
+
+
+PrefiringZAna::~PrefiringZAna()
+{
+}
+
+void
+PrefiringZAna::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
+{
+  using namespace edm;
+
+  Handle<pat::ElectronCollection> electronHandle;
+  iEvent.getByToken(electronToken_, electronHandle);
+  // const auto inputId = electronHandle.id();
+
+  // Handle<ValueMap<bool>> mediumIdHandle;
+  // iEvent.getByToken(mediumIdToken_, mediumIdHandle);
+
+  Handle<reco::VertexCollection> vertexHandle;
+  iEvent.getByToken(vertexToken_, vertexHandle);
+  const auto& pv = vertexHandle->at(0);
+
+  Handle<BXVector<l1t::EGamma>> l1egHandle;
+  iEvent.getByToken(l1egToken_, l1egHandle);
+
+  Handle<pat::TriggerObjectStandAloneCollection> triggerObjectsHandle;
+  iEvent.getByToken(triggerObjectsToken_, triggerObjectsHandle);
+
+  Handle<pat::PackedTriggerPrescales> triggerPrescalesHandle;
+  iEvent.getByToken(triggerPrescalesToken_, triggerPrescalesHandle);
+  const edm::TriggerResults& triggerResults = triggerPrescalesHandle->triggerResults();
+
+
+  pat::Electron * tagEl = nullptr;
+  for(size_t iEl=0; iEl<electronHandle->size(); ++iEl) {
+    // Writeable so cut string can access new data
+    pat::Electron el(electronHandle->at(iEl));
+
+    // el.addUserInt("MediumId", mediumIdHandle->get(inputId, iEl));
+    el.addUserFloat("dxy", el.gsfTrack()->dxy(pv.position()));
+    el.addUserFloat("dz", el.gsfTrack()->dz(pv.position()));
+
+    std::vector<const pat::TriggerObjectStandAlone*> matchedTrigObjs = getMatchedObjs(el.superCluster()->eta(), el.superCluster()->phi(), *triggerObjectsHandle, 0.3);
+    for (const auto trigObjConst : matchedTrigObjs) {
+      pat::TriggerObjectStandAlone trigObj(*trigObjConst);
+      trigObj.unpackNamesAndLabels(iEvent, triggerResults);
+      el.addTriggerObjectMatch(trigObj);
+      int passTrigger = 0;
+      if ( trigObj.hasFilterLabel("hltEle32L1DoubleEGWPTightGsfTrackIsoFilter") && trigObj.hasFilterLabel("hltEGL1SingleEGOrFilter") ) {
+        passTrigger = 1;
+      }
+      el.addUserInt("HLT_Ele32_WPTight_Gsf", passTrigger);
+    }
+
+    // take highest pt electron for now
+    if ( tagEl == nullptr and tagElectronCut_(el) ) {
+      tagEl = &el;
+    }
+  }
+  if ( tagEl == nullptr ) return;
+
+  event_.tag_electron = tagEl->p4();
+
+  event_.l1eg_bx0.clear();
+  for(auto itL1=l1egHandle->begin(0); itL1!=l1egHandle->end(0); ++itL1) {
+    event_.l1eg_bx0.push_back( itL1->p4() );
+  }
+
+  event_.l1eg_bxm1.clear();
+  for(auto itL1=l1egHandle->begin(-1); itL1!=l1egHandle->end(-1); ++itL1) {
+    event_.l1eg_bxm1.push_back( itL1->p4() );
+  }
+
+  event_.l1eg_bxp1.clear();
+  for(auto itL1=l1egHandle->begin(1); itL1!=l1egHandle->end(1); ++itL1) {
+    event_.l1eg_bxp1.push_back( itL1->p4() );
+  }
+
+  tree_->Fill();
+}
+
+
+void
+PrefiringZAna::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
+  //The following says we do not know what parameters are allowed so do no validation
+  // Please change this to state exactly what you do use, even if it is no parameters
+  edm::ParameterSetDescription desc;
+  desc.setUnknown();
+  descriptions.addDefault(desc);
+}
+
+
+DEFINE_FWK_MODULE(PrefiringZAna);
---
+++ test/testPrefiringZ_cfg.py
@@ -0,0 +1,59 @@
+import FWCore.ParameterSet.Config as cms
+
+from FWCore.ParameterSet.VarParsing import VarParsing
+
+process = cms.Process("TEST")
+
+process.load("FWCore.MessageService.MessageLogger_cfi")
+process.MessageLogger.cerr.FwkReport.reportEvery = 1000
+
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring([
+        '/store/data/Run2017F/SingleElectron/MINIAOD/31Mar2018-v1/100000/64244276-8937-E811-AD7C-0CC47A4C8EBA.root',
+    ]),
+    secondaryFileNames = cms.untracked.vstring([
+        '/store/data/Run2017F/SingleElectron/AOD/17Nov2017-v1/60002/D21DD65E-57E0-E711-A3B1-FA163E42C814.root',
+        '/store/data/Run2017F/SingleElectron/AOD/17Nov2017-v1/60002/FAE9BF62-57E0-E711-A80C-FA163E714FAE.root',
+        '/store/data/Run2017F/SingleElectron/AOD/17Nov2017-v1/50000/04F04F9C-82E0-E711-8076-B083FED42A19.root',
+        '/store/data/Run2017F/SingleElectron/AOD/17Nov2017-v1/50000/087E6FA3-79E0-E711-B1A6-141877343E6D.root',
+        '/store/data/Run2017F/SingleElectron/AOD/17Nov2017-v1/50000/0C049678-76E0-E711-8CBF-001EC94BF6C8.root',
+        '/store/data/Run2017F/SingleElectron/AOD/17Nov2017-v1/50000/20BD29AE-DFE0-E711-A962-0025905A48FC.root',
+        '/store/data/Run2017F/SingleElectron/AOD/17Nov2017-v1/50000/7265E7AF-62E0-E711-BE0C-001EC94BF718.root',
+        '/store/data/Run2017F/SingleElectron/AOD/17Nov2017-v1/50000/8A159106-77E0-E711-AB06-1866DAEA6520.root',
+        '/store/data/Run2017F/SingleElectron/AOD/17Nov2017-v1/50000/A8D12096-82E0-E711-82FA-D4AE526567E0.root',
+        '/store/data/Run2017F/SingleElectron/AOD/17Nov2017-v1/50000/B47537BF-79E0-E711-940B-842B2B17E3B8.root',
+        '/store/data/Run2017F/SingleElectron/AOD/17Nov2017-v1/50000/BE2B461A-E6E0-E711-8B42-0CC47A4D760A.root',
+        '/store/data/Run2017F/SingleElectron/AOD/17Nov2017-v1/50000/D2DEFF6F-E2E0-E711-8921-0025905B85B2.root',
+        '/store/data/Run2017F/SingleElectron/AOD/17Nov2017-v1/50001/14805072-6BE0-E711-9323-1866DAEEB0A8.root',
+        '/store/data/Run2017F/SingleElectron/AOD/17Nov2017-v1/50001/641E51D7-62E0-E711-B386-B083FED424C3.root',
+        '/store/data/Run2017F/SingleElectron/AOD/17Nov2017-v1/50001/7E2E8077-69E0-E711-BA3F-1866DAEA6E00.root',
+        '/store/data/Run2017F/SingleElectron/AOD/17Nov2017-v1/50001/9E948648-7AE0-E711-B455-782BCB20BDE3.root',
+        '/store/data/Run2017F/SingleElectron/AOD/17Nov2017-v1/50001/A47C5B15-86E0-E711-93BC-1866DAEA79D4.root',
+        '/store/data/Run2017F/SingleElectron/AOD/17Nov2017-v1/50001/B8E4ABA8-62E0-E711-AAC2-1866DAEA79D4.root',
+        '/store/data/Run2017F/SingleElectron/AOD/17Nov2017-v1/50001/E0D43C81-72E0-E711-A778-B083FED045EC.root',
+        '/store/data/Run2017F/SingleElectron/AOD/17Nov2017-v1/60003/C04573A5-76E0-E711-BC32-FA163EBA1423.root',
+    ]),
+)
+
+process.prefireVetoFilter = cms.EDFilter("TriggerRulePrefireVetoFilter",
+    tcdsRecordLabel = cms.InputTag("tcdsDigis:tcdsRecord"),
+)
+
+process.ntuple = cms.EDAnalyzer("PrefiringZAna",
+    electronSrc = cms.InputTag("slimmedElectrons"),
+    vertexSrc = cms.InputTag("offlineSlimmedPrimaryVertices"),
+    tagElectronCut = cms.string("pt > 30 && abs(eta) < 2.4 && electronID('cutBasedElectronID-Fall17-94X-V1-medium') && userInt('HLT_Ele32_WPTight_Gsf')"),
+    l1egSrc = cms.InputTag("caloStage2Digis:EGamma"),
+    triggerObjects = cms.InputTag("slimmedPatTrigger"),
+    triggerPrescales = cms.InputTag("patTrigger"),
+)
+
+process.skimPath = cms.Path(process.prefireVetoFilter+process.ntuple)
+
+process.TFileService = cms.Service("TFileService",
+    fileName = cms.string("ntuple.root"),
+    closeFileFast = cms.untracked.bool(True)
+)
+
