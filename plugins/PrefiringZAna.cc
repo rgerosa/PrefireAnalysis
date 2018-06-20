bf6316c59fc83a02fea948be9e6df209fc8a5fee
Nick Smith <nick.smith@cern.ch>
2018-06-20 22:39:17 +0200

    Z tuples round 2

--- plugins/PrefiringZAna.cc
+++ plugins/PrefiringZAna.cc
@@ -17,6 +17,7 @@
 #include "CommonTools/Utils/interface/StringCutObjectSelector.h"
 
 #include "DataFormats/PatCandidates/interface/Electron.h"
+#include "DataFormats/EgammaCandidates/interface/Photon.h"
 #include "DataFormats/VertexReco/interface/VertexFwd.h"
 #include "DataFormats/VertexReco/interface/Vertex.h"
 #include "DataFormats/L1Trigger/interface/EGamma.h"
@@ -37,8 +38,15 @@ namespace {
     Long64_t lumi;
     Long64_t event;
     int bunchCrossing;
+    int triggerRule;
     
     LorentzVector tag_electron;
+
+    std::vector<LorentzVector> photon_p4;
+    std::vector<float> photon_sieie;
+    std::vector<float> photon_hoe;
+    std::vector<float> photon_iso;
+
     std::vector<int> L1EG_bx;
     std::vector<LorentzVector> L1EG_p4;
     std::vector<int> L1EG_iso;
@@ -68,10 +76,12 @@ class PrefiringZAna : public edm::one::EDAnalyzer<edm::one::SharedResources>  {
     virtual void analyze(const edm::Event&, const edm::EventSetup&) override;
     // virtual void endJob() override;
 
+    edm::EDGetTokenT<int> triggerRuleToken_;
     edm::EDGetTokenT<pat::ElectronCollection> electronToken_;
     // edm::EDGetTokenT<edm::ValueMap<bool>> mediumIdToken_;
     edm::EDGetTokenT<reco::VertexCollection> vertexToken_;
     StringCutObjectSelector<pat::Electron> tagElectronCut_;
+    edm::EDGetTokenT<reco::PhotonCollection> photonsToken_;
     edm::EDGetTokenT<BXVector<l1t::EGamma>> l1egToken_;
     edm::EDGetTokenT<pat::TriggerObjectStandAloneCollection> triggerObjectsToken_;
     edm::EDGetTokenT<pat::PackedTriggerPrescales> triggerPrescalesToken_;
@@ -81,10 +91,12 @@ class PrefiringZAna : public edm::one::EDAnalyzer<edm::one::SharedResources>  {
 };
 
 PrefiringZAna::PrefiringZAna(const edm::ParameterSet& iConfig):
+  triggerRuleToken_(consumes<int>(iConfig.getParameter<edm::InputTag>("triggerRule"))),
   electronToken_(consumes<pat::ElectronCollection>(iConfig.getParameter<edm::InputTag>("electronSrc"))),
   //  mediumIdToken_(consumes<edm::ValueMap<bool>>(iConfig.getParameter<edm::InputTag>("mediumId"))),
   vertexToken_(consumes<reco::VertexCollection>(iConfig.getParameter<edm::InputTag>("vertexSrc"))),
   tagElectronCut_(iConfig.getParameter<std::string>("tagElectronCut")),
+  photonsToken_(consumes<reco::PhotonCollection>(iConfig.getParameter<edm::InputTag>("photonSrc"))),
   l1egToken_(consumes<BXVector<l1t::EGamma>>(iConfig.getParameter<edm::InputTag>("l1egSrc"))),
   triggerObjectsToken_(consumes<pat::TriggerObjectStandAloneCollection>(iConfig.getParameter<edm::InputTag>("triggerObjects"))),
   triggerPrescalesToken_(consumes<pat::PackedTriggerPrescales>(iConfig.getParameter<edm::InputTag>("triggerPrescales")))
@@ -97,7 +109,12 @@ PrefiringZAna::PrefiringZAna(const edm::ParameterSet& iConfig):
   tree_->Branch("lumi", &event_.lumi);
   tree_->Branch("event", &event_.event);
   tree_->Branch("bunchCrossing", &event_.bunchCrossing);
+  tree_->Branch("triggerRule", &event_.triggerRule);
   tree_->Branch("tag_electron", &event_.tag_electron);
+  tree_->Branch("photon_p4", &event_.photon_p4);
+  tree_->Branch("photon_sieie", &event_.photon_sieie);
+  tree_->Branch("photon_hoe", &event_.photon_hoe);
+  tree_->Branch("photon_iso", &event_.photon_iso);
   tree_->Branch("L1EG_bx", &event_.L1EG_bx);
   tree_->Branch("L1EG_p4", &event_.L1EG_p4);
   tree_->Branch("L1EG_iso", &event_.L1EG_iso);
@@ -118,6 +135,10 @@ PrefiringZAna::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
   event_.event = iEvent.id().event();
   event_.bunchCrossing = iEvent.bunchCrossing();
 
+  Handle<int> triggerRuleHandle;
+  iEvent.getByToken(triggerRuleToken_, triggerRuleHandle);
+  event_.triggerRule = *triggerRuleHandle;
+
   Handle<pat::ElectronCollection> electronHandle;
   iEvent.getByToken(electronToken_, electronHandle);
   // const auto inputId = electronHandle.id();
@@ -128,6 +149,9 @@ PrefiringZAna::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
   Handle<reco::VertexCollection> vertexHandle;
   iEvent.getByToken(vertexToken_, vertexHandle);
   const auto& pv = vertexHandle->at(0);
+
+  Handle<reco::PhotonCollection> photonsHandle;
+  iEvent.getByToken(photonsToken_, photonsHandle);
 
   Handle<BXVector<l1t::EGamma>> l1egHandle;
   iEvent.getByToken(l1egToken_, l1egHandle);
@@ -150,17 +174,23 @@ PrefiringZAna::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
     el.addUserFloat("dz", el.gsfTrack()->dz(pv.position()));
 
     std::vector<const pat::TriggerObjectStandAlone*> matchedTrigObjs = getMatchedObjs(el.superCluster()->eta(), el.superCluster()->phi(), *triggerObjectsHandle, 0.3);
+    int pass2016trigger{0};
+    int pass2017trigger{0};
     for (const auto trigObjConst : matchedTrigObjs) {
       pat::TriggerObjectStandAlone trigObj(*trigObjConst);
       trigObj.unpackNamesAndLabels(iEvent, triggerResults);
-      el.addTriggerObjectMatch(trigObj);
-      int passTrigger = 0;
-      // TODO: can't use with 2016 data
-      if ( trigObj.hasFilterLabel("hltEle32L1DoubleEGWPTightGsfTrackIsoFilter") && trigObj.hasFilterLabel("hltEGL1SingleEGOrFilter") ) {
-        passTrigger = 1;
+
+      // HLT_Ele25_eta2p1_WPTight_Gsf_v OR HLT_Ele27_WPTight_Gsf_v
+      if ( trigObj.hasFilterLabel("hltEle25erWPTightGsfTrackIsoFilter") || trigObj.hasFilterLabel("hltEle27WPTightGsfTrackIsoFilter") ) {
+        pass2016trigger = 1;
       }
-      el.addUserInt("HLT_Ele32_WPTight_Gsf", passTrigger);
+      // HLT_Ele32_WPTight_Gsf with special recipe
+      if ( trigObj.hasFilterLabel("hltEle32L1DoubleEGWPTightGsfTrackIsoFilter") && trigObj.hasFilterLabel("hltEGL1SingleEGOrFilter") ) {
+        pass2017trigger = 1;
+      }
     }
+    el.addUserInt("pass2016trigger", pass2016trigger);
+    el.addUserInt("pass2017trigger", pass2017trigger);
 
     // take highest pt electron for now
     if ( tagEl == nullptr and tagElectronCut_(el) ) {
@@ -170,6 +200,21 @@ PrefiringZAna::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
   if ( tagEl == nullptr ) return;
 
   event_.tag_electron = tagEl->p4();
+
+
+  event_.photon_p4.clear();
+  event_.photon_sieie.clear();
+  event_.photon_hoe.clear();
+  event_.photon_iso.clear();
+  for(const auto& pf : *photonsHandle) {
+    if ( pf.energy() < 10. ) {
+      continue;
+    }
+    event_.photon_p4.push_back(pf.p4());
+    event_.photon_sieie.push_back(pf.sigmaIetaIeta());
+    event_.photon_hoe.push_back(pf.hadronicOverEm());
+    event_.photon_iso.push_back(pf.neutralHadronIso()+pf.photonIso());
+  }
 
 
   event_.L1EG_bx.clear();
